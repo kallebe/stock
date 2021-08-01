2f189010786e7ec6d89056a33133fdcb7e1c6e8b
Kallebe Sousa <kallebe87@gmail.com>
2021-08-01 10:22:42 -0300

    :sparkles: Order CSV by date and type

--- app/controllers/movimentacaos_controller.rb
+++ app/controllers/movimentacaos_controller.rb
@@ -20,8 +20,8 @@ class MovimentacaosController < ApplicationController
   def order_csv
     @csv_data = CSV.parse(File.read(params[:movimentacao_csv], headers: true))
 
-    # Sort CSV by its date
-    @csv_data = @csv_data.sort { |a, b| a[1].to_date <=> b[1].to_date }
+    # Sort CSV by its date and then sorting Entries first
+    @csv_data = @csv_data.sort { |a, b| [a[1].to_date, a[2]] <=> [b[1].to_date, b[2]] }
   end
 
   def create_from_csv
